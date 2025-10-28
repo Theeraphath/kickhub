7c2905b29c2755f33103bd82d4f28d04266b3441
aomsinspu <saral.nit@spumail.net>
2025-10-28 12:33:43 +0700

    สร้าง navbar

--- frontend/kickhub/src/App.jsx
+++ frontend/kickhub/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import BottomNav from "./components/Navbar";
 import Login from "./components/Login";
 import Test1 from "./components/test";
 import Home from "./components/home";
@@ -9,20 +10,13 @@ import Promptpay from "./components/prompypay";
 function App() {
   return (
     <Router>
-      {/* Simple navigation bar กด comments ถ้าจะแต่ง css เพิ่ม */}
-      {/* <div className="p-4 bg-gray-200 flex items-center justify-center">
-        <nav className="space-x-4 text-lg font-semibold text-gray-700">
-          <Link to="/">Home</Link>
-          <Link to="/reserve">Reserve</Link>
-          <Link to="/prompypay">Promptpay</Link>
-        </nav>
-      </div> */}
       <Routes>
         <Route path="/" element={<Test1 />} />
         <Route path="/reserve" element={<Reserve />} />
         <Route path="/Login" element={<Login />} />
-        <Route path="prompypay" element={<Promptpay />} />
+        <Route path="/prompypay" element={<Promptpay />} />
       </Routes>
+      <BottomNav />
     </Router>
   );
 }
