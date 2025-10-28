99ce9a5a6ff5f07b22b68ea90cfc1030c4e44cec
aomsinspu <saral.nit@spumail.net>
2025-10-28 11:34:12 +0700

    promptpay

--- frontend/kickhub/src/App.jsx
+++ frontend/kickhub/src/App.jsx
@@ -4,21 +4,24 @@ import Test1 from "./components/test";
 import Home from "./components/home";
 import Party from "./components/party";
 import Reserve from "./components/reserve";
+import Promptpay from "./components/prompypay";
 
 function App() {
   return (
     <Router>
-       {/* Simple navigation bar กด comments ถ้าจะแต่ง css เพิ่ม */}
+      {/* Simple navigation bar กด comments ถ้าจะแต่ง css เพิ่ม */}
       {/* <div className="p-4 bg-gray-200 flex items-center justify-center">
         <nav className="space-x-4 text-lg font-semibold text-gray-700">
           <Link to="/">Home</Link>
           <Link to="/reserve">Reserve</Link>
-        </nav> 
+          <Link to="/prompypay">Promptpay</Link>
+        </nav>
       </div> */}
       <Routes>
         <Route path="/" element={<Test1 />} />
         <Route path="/reserve" element={<Reserve />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="prompypay" element={<Promptpay />} />
       </Routes>
     </Router>
   );
---
+++ frontend/kickhub/src/components/prompypay.jsx
@@ -0,0 +1,46 @@
+// import { React, useState } from "react";
+import promptpaylogo from "../../public/prompt-pay-logo.svg";
+import Qrcode from "../../public/googleQRcodes.png";
+
+export default function Promptpay() {
+  const dummydata = {
+    referenceNumber: "123456",
+    amount: "700",
+    telaphoneNumber: "012-345-6789",
+    fieldName: "สนามฟุตบอลศรีปทุม",
+  };
+
+  return (
+    <div className="bg-white rounded-lg shadow-md max-w-md mx-auto mt-10 font-noto-thai flex flex-col items-center">
+      <h1 className="text-2xl font-bold">QR PromptPay</h1>
+      <div className="bg-green-300 p-4 mt-4 w-full flex flex-col items-center">
+        <p>บันทึก QR Code นี้และนำไปสแกนผ่านแอปธนาคาร</p>
+      </div>
+      <div className="bg-gray-200 p-4 w-full grid grid-cols-2 justify-between">
+        <p>เลขที่อ้างอิง</p>
+        <p className="place-self-end">{dummydata.referenceNumber}</p>
+        <p>จำนวนเงินที่ต้องชำระ</p>
+        <p className="place-self-end">{dummydata.amount}</p>
+      </div>
+      <div className="flex flex-col items-center w-[17rem]">
+        <img
+          src={promptpaylogo}
+          alt="PromptPay QR Code"
+          className="mt-4 mb-4"
+        />
+        <img src={Qrcode} alt="QR Code for Payment" className="w-full mb-4" />
+      </div>
+      <div className="flex flex-col items-center">
+        <p>หมายเลขพร้อมเพย์</p>
+        <p className="font-bold">{dummydata.telaphoneNumber}</p>
+        <p>{dummydata.amount} บาท</p>
+        <p>{dummydata.fieldName}</p>
+      </div>
+      <div className="flex flex-col items-center w-[10rem]">
+        <button className="bg-green-200 text-black w-full p-3 rounded-lg mt-4 mb-4 hover:bg-green-300 border border-green-400 cursor-pointer">
+          บันทึก QR Code
+        </button>
+      </div>
+    </div>
+  );
+}
