b49ca2dacb5c8fe5ee6b89321b40be78c5e6bcbf
ChaosMAX <saral.nit@spumail,net>
2025-11-04 17:21:58 +0700

    add notifications

--- frontend/kickhub/src/App.jsx
+++ frontend/kickhub/src/App.jsx
@@ -14,13 +14,10 @@ import Promptpay from "./components/prompypay";
 import ProtectedRoute from "./components/ProtectedRoute";
 import SignUp from "./components/SignUp";
 import Partybuffet from "./components/Partybuffet";
-
- import FindCreateParty from "./components/FindCreateParty";
- import FindandCreate from "./components/FindandCreate";
-
-
+import FindCreateParty from "./components/FindCreateParty";
+import FindandCreate from "./components/FindandCreate";
+import Notifications from "./components/Notification";
 import Test2 from "./components/test2";
-
 
 function AppRoutes() {
   const location = useLocation();
@@ -40,11 +37,10 @@ function AppRoutes() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={<Home />} />
         <Route path="/partybuffet" element={<Partybuffet />} />
-
-        <Route path="/team" element={<FindCreateParty />} />        <Route path="/findandcreate" element={<FindandCreate />} />
-
+        <Route path="/team" element={<FindCreateParty />} />
+        <Route path="/findandcreate" element={<FindandCreate />} />
         <Route path="/test/:id" element={<Test2 />} />
-
+        <Route path="/notifications" element={<Notifications />} />
 
         <Route
           path="/test"
