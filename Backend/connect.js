53ce83b84a3408a8ed03d640b8c07f4e2d2bf413
theerapat saiubol <jamintheeraphat@gmail.com>
2025-11-09 16:12:11 +0700

    profile

---
+++ Backend/connect.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/kickhub", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
